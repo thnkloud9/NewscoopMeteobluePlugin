9d593993d286c8efee1630bf0e98d4de1d767d26
Mark Lewis <thnkloud9@gmail.com>
2013-11-28 14:06:55 -0300

    update job to use new etity for stats

--- Command/UpdateWeatherCommand.php
+++ Command/UpdateWeatherCommand.php
@@ -98,7 +98,7 @@ class UpdateWeatherCommand extends ContainerAwareCommand
         
         $todayKeys = array_keys($data, $date);
        
-        $em = $this->getDoctrine()->getManager();
+        $em = $this->getContainer()->get('doctrine')->getEntityManager();
         $meteoblueStatRepo = $em->getRepository('Newscoop\MeteobluePluginBundle\Entity\MeteoblueStat');
  
         foreach ($todayKeys as $todayKey) {

00f71692bae7b8a3c95790c34d8e13ce6f2d2602
Mark Lewis <thnkloud9@gmail.com>
2013-11-28 14:19:25 -0300

    update job to use new etity for stats

--- Command/UpdateWeatherCommand.php
+++ Command/UpdateWeatherCommand.php
@@ -108,24 +108,16 @@ class UpdateWeatherCommand extends ContainerAwareCommand
             $icon = $icons[$data[$todayKey + 7]];
 
             // save the temp
-            if ($meteoblueStatRepo->countBy(array('option' => 'weather_temperature_'.$hour)) > 0) {
-                $tempStat = $em->getRepository('Newscoop\MeteobluePluginBundle\Entity\MeteoblueStat')
-                    ->findOneBy(array(
-                        'option' => 'weather_temperature_'.$hour,
-                    ));
-            } else {
+            $tempStat = $meteoblueStatRepo->findOneBy(array('option' => 'weather_temperature_'.$hour)) > 0) {
+            if (!$tempStat) {
                 $tempStat = new MeteoblueStat();
                 $tempStat->setOption('weather_temperature_'.$hour);
             }
             $tempStat->setValue($temperature);
 
             // save the icon 
-            if ($meteoblueStatRepo->countBy(array('option' => 'weather_icon_'.$hour)) > 0) {
-                $iconStat = $em->getRepository('Newscoop\MeteobluePluginBundle\Entity\MeteoblueStat')
-                    ->findOneBy(array(
-                        'id' => 'weather_icon_'.$hour,
-                    ));
-            } else {
+            $iconStat = $meteoblueStatRepo->countBy(array('option' => 'weather_icon_'.$hour)) > 0) {
+            if (!$iconStat) {
                 $iconStat = new MeteoblueStat();
                 $iconStat->setOption('weather_icon_'.$hour);
             }

c75d082b3ef4e5c1a0f4789c5f14b65b3f21833f
Mark Lewis <thnkloud9@gmail.com>
2013-12-03 13:56:55 -0300

    adding smarty function for details output

--- Resources/smartyPlugins/function.meteoblue_details.php
+++ Resources/smartyPlugins/function.meteoblue_details.php
@@ -12,6 +12,6 @@
  */
 function smarty_function_meteoblue_details(array $params, Smarty_Internal_Template $smarty)
 {
-    return sprintf('<iframe src="http://www.meteoblue.com/de_DE/customer/tageswoche/index/%s" height="%d" width="%d" frameborder="0" scrolling="NO"></iframe><div><a href="http://www.meteoblue.com/de_DE/plugin/meteoblue/details/vorhersage/woche/%s?utm_source=tageswoche&utm_medium=linkus&utm_campaign=CustomWidget" title="Wetter Basel - meteoblue" target="_blank">meteoblue.com</a></div>', $params['citycode'], $params['height'], $params['width'], $params['citycode']);
+    return sprintf('<iframe src="https://www.meteoblue.com/de_DE/customer/tageswoche/index/%s" height="%d" width="%d" frameborder="0" scrolling="NO"></iframe><div><a href="http://www.meteoblue.com/de_DE/plugin/meteoblue/details/vorhersage/woche/%s?utm_source=tageswoche&utm_medium=linkus&utm_campaign=CustomWidget" title="Wetter Basel - meteoblue" target="_blank">meteoblue.com</a></div>', $params['citycode'], $params['height'], $params['width'], $params['citycode']);
 
 }
